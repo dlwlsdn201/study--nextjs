3dedac162715fd42fc3bea35ce0cd819ed2bda76
jinWoo_Lee <dlwlsdn201@naver.com>
2022-03-10 22:33:48 +0900

    [documentation] 1. test page 생성 + 2. READ ME 업데이트

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
 import '../styles/globals.css';
 import GlobalStyle from '../styles/GlobalStyle.ts';
 import { theme } from '../styles/Theme.ts';
-import '../styles/GlobalStyle.ts';
 import { ThemeProvider } from 'styled-components';
+import Test from './test';
+
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Component {...pageProps} />;
+      <Test />
     </ThemeProvider>
   );
 }
