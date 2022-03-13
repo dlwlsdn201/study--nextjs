88befb886b708d33869eb4bc16249001bd34d163
jinWoo_Lee <dlwlsdn201@naver.com>
2022-03-13 17:30:50 +0900

    [enhancement] 1. 화장품 상품 api를 이용하여 data 출력

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['d3t32hsnjxo7q6.cloudfront.net']
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
