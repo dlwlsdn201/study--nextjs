1b6d7bd23d1a20d9f188df25038ce4a542e98a9c
jinWoo_Lee <dlwlsdn201@naver.com>
2022-03-14 22:24:16 +0900

    [enhancement] 1. axois 라이브러리 설치 + 2. 화장품 상품 api 이용하여 동적 라우팅 기능 실습

---
+++ src/components/ItemList.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Grid, Image } from 'semantic-ui-react';
+import styles from '../../styles/ItemList.module.css';
+import Link from 'next/link';
+interface product {
+  image_link: string;
+  name: string;
+  price: string;
+  id: number;
+  product_link: string;
+  brand: string;
+}
+
+const ItemList = ({ list }) => {
+  return (
+    <Grid columns={3} divided>
+      <Grid.Row>
+        {list.map((item: product, index: number) => (
+          <Grid.Column key={index}>
+            <Link href={`/view/${item.id}`}>
+              <a>
+                <Image
+                  alt={item.name}
+                  src={item.image_link}
+                  className={styles.img_item}
+                />
+              </a>
+            </Link>
+            <strong>{item.name}</strong>
+            <strong>{item.price}</strong>
+          </Grid.Column>
+        ))}
+      </Grid.Row>
+    </Grid>
+  );
+};
+
+export default ItemList;
