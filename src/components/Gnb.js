db5d389af782fc127d4b968b1786a21f18efe095
jinWoo_Lee <dlwlsdn201@naver.com>
2022-03-16 22:07:47 +0900

    [enhancement] 1. Navigation 메뉴에 라우팅 기능 구현

--- src/components/Gnb.js
+++ src/components/Gnb.js
@@ -1,24 +1,32 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
 
 const Gnb = () => {
-  const activeItem = 'home';
+  const router = useRouter();
+  let activeItem;
+
+  if (router.pathname === '/') {
+    activeItem = 'Home';
+  } else if (router.pathname === '/about') {
+    activeItem = 'about';
+  }
+
+  const goLink = (e, data) => {
+    if (data.name === 'home') {
+      router.push('/');
+    } else if (data.name === 'about') {
+      router.push('/about');
+    }
+  };
+
   return (
     <Menu inverted>
+      <Menu.Item name="home" active={activeItem === 'home'} onClick={goLink} />
       <Menu.Item
-        name="home"
-        active={activeItem === 'home'}
-        // onClick={this.handleItemClick}
-      />
-      <Menu.Item
-        name="messages"
-        active={activeItem === 'messages'}
-        // onClick={this.handleItemClick}
-      />
-      <Menu.Item
-        name="friends"
-        active={activeItem === 'friends'}
-        // onClick={this.handleItemClick}
+        name="about"
+        active={activeItem === 'about'}
+        onClick={goLink}
       />
     </Menu>
   );
